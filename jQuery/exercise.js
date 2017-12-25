09aef6d44dd1a0a98f9667b71352a94ad20b55c3
suyogdaga <suyogdaga@gmail.com>
2017-12-25 15:06:39 -0500

    Jquery tutorial done!

---
+++ jQuery/exercise.js
@@ -0,0 +1,17 @@
+$("div").css("background-color","purple");
+
+var style= {
+	width : "200px"
+}
+
+var style1 = {
+	 border : "2px solid orange "
+}
+
+
+$("div.highlight").css(style);
+
+
+$("#third").css(style1);
+
+$("div:first").css("color","pink")
